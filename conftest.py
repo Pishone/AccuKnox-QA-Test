fb49a459309665866de7bad344c8fcbb8917dd52
Pishone David <r.pishone@gmail.com>
2024-06-29 17:02:23 +0530

    Add driver and fixture

--- conftest.py
+++ conftest.py
@@ -1,3 +1,14 @@
 import pytest
+from selenium import webdriver
 
-def 
+@pytest.fixture(scope="module")
+def driver():
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+@pytest.fixture(scope="function", autouse=True)
+def login(driver):
+    driver.get("http://192.168.39.133:32499/")
+    
